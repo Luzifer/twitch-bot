a406df9c74897eb8c8f01f1c5a515579408912de
Knut Ahlers <knut@ahlers.me>
2021-08-19 01:38:09 +0200

    Add example plugin
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

---
+++ examples/plugin/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Luzifer/twitch-bot/plugins"
+	"github.com/go-irc/irc"
+	"github.com/pkg/errors"
+)
+
+var _ plugins.RegisterFunc = Register
+
+func Register(args plugins.RegistrationArguments) error {
+	args.GetLogger("plugin-example").Warn("Example Register called")
+
+	args.RegisterActor(func() plugins.Actor { return &actor{} })
+
+	return nil
+}
+
+type actor struct {
+	Example bool `json:"example" yaml:"example"`
+}
+
+func (a actor) Execute(c *irc.Client, m *irc.Message, r *plugins.Rule) (preventCooldown bool, err error) {
+	if !a.Example {
+		return false, nil
+	}
+
+	return false, errors.Wrap(
+		c.WriteMessage(&irc.Message{
+			Command: "PRIVMSG",
+			Params: []string{
+				m.Params[0],
+				fmt.Sprintf("@%s Example plugin noticed you! KonCha", m.User),
+			},
+		}),
+		"sending response",
+	)
+}
+
+func (a actor) IsAsync() bool { return false }
+func (a actor) Name() string  { return "example" }
