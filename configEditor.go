eec895918269d78783e353acc6a126d4259526bf
Knut Ahlers <knut@ahlers.me>
2021-09-18 22:59:39 +0200

    Add cron validator
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- configEditor.go
+++ configEditor.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"regexp"
 	"sync"
+	"time"
 
 	"github.com/Luzifer/twitch-bot/plugins"
 	log "github.com/sirupsen/logrus"
@@ -51,6 +52,42 @@ func init() {
 			Module:       "config-editor",
 			Name:         "Get current configuration",
 			Path:         "/rules",
+			ResponseType: plugins.HTTPRouteResponseTypeJSON,
+		},
+		{
+			Description: "Validate a cron expression and return the next executions",
+			HandlerFunc: func(w http.ResponseWriter, r *http.Request) {
+				sched, err := cronParser.Parse(r.FormValue("cron"))
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusBadRequest)
+					return
+				}
+
+				var (
+					lt  = time.Now()
+					out []time.Time
+				)
+				for i := 0; i < 3; i++ {
+					lt = sched.Next(lt)
+					out = append(out, lt)
+				}
+
+				if err := json.NewEncoder(w).Encode(out); err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+				}
+			},
+			Method: http.MethodPut,
+			Module: "config-editor",
+			Name:   "Validate cron expression",
+			Path:   "/validate-cron",
+			QueryParams: []plugins.HTTPRouteParamDocumentation{
+				{
+					Description: "The cron expression to test",
+					Name:        "cron",
+					Required:    true,
+					Type:        "string",
+				},
+			},
 			ResponseType: plugins.HTTPRouteResponseTypeJSON,
 		},
 		{
