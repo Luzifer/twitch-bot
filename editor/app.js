61f20f421446cd6bd12a51517ccc0eefe00049cd
Knut Ahlers <knut@ahlers.me>
2021-10-23 00:55:12 +0200

    Add frontend for API-Token management
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- editor/app.js
+++ editor/app.js
@@ -43,6 +43,13 @@ new Vue({
       ]
     },
 
+    availableModules() {
+      return [
+        { text: 'ANY', value: '*' },
+        ...this.modules.map(m => ({ text: m, value: m })),
+      ]
+    },
+
     axiosOptions() {
       return {
         headers: {
@@ -93,6 +100,10 @@ new Vue({
 
           return an.localeCompare(bn)
         })
+    },
+
+    validateAPIToken() {
+      return this.models.apiToken.modules.length > 0 && Boolean(this.models.apiToken.name)
     },
 
     validateAutoMessage() {
@@ -183,6 +194,7 @@ new Vue({
 
   data: {
     actions: [],
+    apiTokens: {},
     authToken: null,
     autoMessageFields: [
       {
@@ -221,6 +233,7 @@ new Vue({
     configNotifySocket: null,
     configNotifySocketConnected: false,
     configNotifyBackoff: 100,
+    createdAPIToken: null,
     editMode: 'general',
     error: null,
     generalConfig: {},
@@ -228,10 +241,12 @@ new Vue({
       addAction: '',
       addChannel: '',
       addEditor: '',
+      apiToken: {},
       autoMessage: {},
       rule: {},
     },
 
+    modules: [],
     rules: [],
     rulesFields: [
       {
@@ -254,6 +269,7 @@ new Vue({
       },
     ],
 
+    showAPITokenEditModal: false,
     showAutoMessageEditModal: false,
     showRuleEditModal: false,
     userProfiles: {},
@@ -344,6 +360,14 @@ new Vue({
         .catch(err => this.handleFetchError(err))
     },
 
+    fetchAPITokens() {
+      return axios.get('config-editor/auth-tokens', this.axiosOptions)
+        .then(resp => {
+          this.apiTokens = resp.data
+        })
+        .catch(err => this.handleFetchError(err))
+    },
+
     fetchAutoMessages() {
       return axios.get('config-editor/auto-messages', this.axiosOptions)
         .then(resp => {
@@ -365,6 +389,14 @@ new Vue({
 
           return Promise.all(promises)
         })
+    },
+
+    fetchModules() {
+      return axios.get('config-editor/modules')
+        .then(resp => {
+          this.modules = resp.data
+        })
+        .catch(err => this.handleFetchError(err))
     },
 
     fetchProfile(user) {
@@ -500,6 +532,14 @@ new Vue({
       Vue.set(this.models.rule, 'actions', tmp)
     },
 
+    newAPIToken() {
+      Vue.set(this.models, 'apiToken', {
+        name: '',
+        modules: [],
+      })
+      this.showAPITokenEditModal = true
+    },
+
     newAutoMessage() {
       Vue.set(this.models, 'autoMessage', {})
       this.showAutoMessageEditModal = true
@@ -545,6 +585,7 @@ new Vue({
 
     reload() {
       return Promise.all([
+        this.fetchAPITokens(),
         this.fetchAutoMessages(),
         this.fetchGeneralConfig(),
         this.fetchRules(),
@@ -555,6 +596,14 @@ new Vue({
 
     removeAction(idx) {
       this.models.rule.actions = this.models.rule.actions.filter((_, i) => i !== idx)
+    },
+
+    removeAPIToken(uuid) {
+      axios.delete(`config-editor/auth-tokens/${uuid}`, this.axiosOptions)
+        .then(() => {
+          this.changePending = true
+        })
+        .catch(err => this.handleFetchError(err))
     },
 
     removeChannel(channel) {
@@ -571,9 +620,27 @@ new Vue({
       this.updateGeneralConfig()
     },
 
+    saveAPIToken() {
+      if (!this.validateAPIToken) {
+        evt.preventDefault()
+        return
+      }
+
+      axios.post(`config-editor/auth-tokens`, this.models.apiToken, this.axiosOptions)
+        .then(resp => {
+          this.createdAPIToken = resp.data
+          this.changePending = true
+          window.setTimeout(() => {
+            this.createdAPIToken = null
+          }, 30000)
+        })
+        .catch(err => this.handleFetchError(err))
+    },
+
     saveAutoMessage(evt) {
       if (!this.validateAutoMessage) {
         evt.preventDefault()
+        return
       }
 
       const obj = { ...this.models.autoMessage }
@@ -602,6 +669,7 @@ new Vue({
     saveRule(evt) {
       if (!this.validateRule) {
         evt.preventDefault()
+        return
       }
 
       const obj = {
@@ -786,6 +854,7 @@ new Vue({
   mounted() {
     this.fetchVars()
     this.fetchActions()
+    this.fetchModules()
 
     const params = new URLSearchParams(window.location.hash.substring(1))
     this.authToken = params.get('access_token') || null
