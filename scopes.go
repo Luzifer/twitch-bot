be7aadbaf113dc857de8b66fd8c87abe59ae89c3
Knut Ahlers <knut@ahlers.me>
2022-12-04 16:58:31 +0100

    Add more scopes to extended scopes
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- scopes.go
+++ scopes.go
@@ -4,11 +4,13 @@ import "github.com/Luzifer/twitch-bot/v3/pkg/twitch"
 
 var (
 	channelExtendedScopes = map[string]string{
-		twitch.ScopeChannelEditCommercial:  "run commercial",
-		twitch.ScopeChannelManageBroadcast: "modify category / title",
-		twitch.ScopeChannelManageRaids:     "start raids",
-		twitch.ScopeChannelManageVIPS:      "add VIPs",
-		twitch.ScopeChannelReadRedemptions: "see channel-point redemptions",
+		twitch.ScopeChannelEditCommercial:    "run commercial",
+		twitch.ScopeChannelManageBroadcast:   "modify category / title",
+		twitch.ScopeChannelManagePolls:       "manage polls",
+		twitch.ScopeChannelManagePredictions: "manage predictions",
+		twitch.ScopeChannelManageRaids:       "start raids",
+		twitch.ScopeChannelManageVIPS:        "manage VIPs",
+		twitch.ScopeChannelReadRedemptions:   "see channel-point redemptions",
 	}
 
 	botDefaultScopes = []string{
