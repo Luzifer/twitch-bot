0a82b36bccd6a64afe080e1b0dd4f77e13a14db9
Knut Ahlers <knut@ahlers.me>
2024-06-13 18:58:57 +0200

    Add toasts to communicate with user
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

---
+++ src/helpers/busevents.ts
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+
+enum BusEventTypes {
+  ChangePending = 'changePending',
+  ConfigReload = 'configReload',
+  Error = 'error',
+  FetchError = 'fetchError',
+  LoadingData = 'loadingData',
+  LoginProcessing = 'loginProcessing',
+  Toast = 'toast',
+}
+
+export default BusEventTypes
