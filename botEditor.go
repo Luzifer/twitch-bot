1a425416b897ab66c92f979b7299e5fdafea3a11
Knut Ahlers <knut@ahlers.me>
2024-08-14 15:53:09 +0200

    Add channel editing interface
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- botEditor.go
+++ botEditor.go
@@ -3,21 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/pkg/errors"
+	"strings"
 )
 
 func getAuthorizationFromRequest(r *http.Request) (string, error) {
-	token := r.Header.Get("Authorization")
-	if token == "" {
+	_, token, hadPrefix := strings.Cut(r.Header.Get("Authorization"), " ")
+	if !hadPrefix {
 		return "", fmt.Errorf("no authorization provided")
 	}
 
 	_, user, _, _, err := editorTokenService.ValidateLoginToken(token) //nolint:dogsled // Required at other places
+	if err != nil {
+		return "", fmt.Errorf("getting authorized user: %w", err)
+	}
 
 	if user == "" {
 		user = "API-User"
 	}
 
-	return user, errors.Wrap(err, "getting authorized user")
+	return user, nil
 }
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHashHistory, type RouteRecordRaw } from 'vue-router'
 
 import BothAuth from './components/botauth.vue'
+import ChannelOverview from './components/channelOverview.vue'
+import ChannelPermissions from './components/channelPermissions.vue'
 import Dashboard from './components/dashboard.vue'
 
 const routes = [
@@ -8,23 +10,24 @@ const routes = [
 
   // General settings
   { component: BothAuth, name: 'botAuth', path: '/bot-auth' },
-  { component: {}, name: 'channels', path: '/channels' },
+  { component: ChannelOverview, name: 'channels', path: '/channels' },
+  { component: ChannelPermissions, name: 'channelPermissions', path: '/channels/:channel/permissions', props: true },
   { component: {}, name: 'editors', path: '/editors' },
   { component: {}, name: 'tokens', path: '/tokens' },
 
   // Auto-Messages
   { component: {}, name: 'autoMessagesList', path: '/auto-messages' },
-  { component: {}, name: 'autoMessageEdit', path: '/auto-messages/edit/{id}' },
+  { component: {}, name: 'autoMessageEdit', path: '/auto-messages/edit/:id' },
   { component: {}, name: 'autoMessageNew', path: '/auto-messages/new' },
 
   // Rules
   { component: {}, name: 'rulesList', path: '/rules' },
-  { component: {}, name: 'rulesEdit', path: '/rules/edit/{id}' },
+  { component: {}, name: 'rulesEdit', path: '/rules/edit/:id' },
   { component: {}, name: 'rulesNew', path: '/rules/new' },
 
   // Raffles
   { component: {}, name: 'rafflesList', path: '/raffles' },
-  { component: {}, name: 'rafflesEdit', path: '/raffles/edit/{id}' },
+  { component: {}, name: 'rafflesEdit', path: '/raffles/edit/:id' },
   { component: {}, name: 'rafflesNew', path: '/raffles/new' },
 ] as RouteRecordRaw[]
 
